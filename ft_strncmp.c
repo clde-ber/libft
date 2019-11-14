9fc6a1b05d80fbeacad8058626b572388e853fb2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-14 16:37:43 +0100

    Create ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (s1[i] || s2[i])
+    {
+        if ((s1[i] != s2[i]) || (i == n))
+            break;
+        i++;
+    }
+    return (s1[i] - s2[i]);
+}
+
+int     main(int ac, char **av)
+{
+    printf("%d", ft_strncmp(av[1], av[2], 3));
+    return (0);
+}
