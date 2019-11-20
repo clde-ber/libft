e8ce80b4e848d225d7188beec405599546c0aff2
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:32:01 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,19 +1,22 @@
-char    *ft_strchr(const char *s, int c)
-{
-    int i;
+#include <stdio.h>
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == (char)c)
-            return ((char *)&s[i]);
-        i++;
-    }
-    return (0);
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (0);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf("%s", ft_strchr(av[1], av[2][0]));
-    return (0);
+	(void)ac;
+	printf("%s", ft_strchr(av[1], av[2][0]));
+	return (0);
 }
