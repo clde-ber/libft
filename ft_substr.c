ebcac7ff744eb7682717363b147adf22020e1faf
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:41:19 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -1,29 +1,42 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+static int	ft_strlen(char *str)
 {
-    size_t i;
-    size_t j;
-    char *ptr;
+	size_t	i;
 
-    i = 0;
-    if(!(ptr = (char*)malloc(sizeof(s))))
-        return (0);
-    while (i < len)
-    {
-        if (i >= start)
-        {
-            ptr[j] = s[i];
-            j++;
-        }
-        i++;
-    }
-    return (ptr);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-int main(int ac, char **av)
+char		*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    printf("%s", ft_substr(av[1], 5, 19));
-    return (0);
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	if (!(ptr = (char*)malloc(ft_strlen((char *)s - start) + 1)))
+		return (0);
+	while (i < len)
+	{
+		if (i >= start)
+		{
+			ptr[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	return (ptr);
+}
+
+int		main(int ac, char **av)
+{
+	(void)ac;
+	printf("%s", ft_substr(av[1], 3, 19));
+	return (0);
 }
