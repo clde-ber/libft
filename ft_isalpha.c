5acc7f01b9fff73c0975c49b7bf7df2493b3267b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:46:05 +0100

    Create ft_isalpha.c

---
+++ ft_isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	ft_isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d", ft_isalpha(0));
+	return (0);
+}
