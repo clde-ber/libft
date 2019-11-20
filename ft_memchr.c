29698254573140c9fdfae449a9eefcbfe6e07ab1
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:26:16 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,21 +1,22 @@
 #include <string.h>
+#include <stdio.h>
 
-void    *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (i < n)
-    {
-        if (((char *)s)[i] == c)
-            return (&s[i]);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (i < n)
+	{
+		if (((char *)s)[i] == c)
+			return ((void *)&s[i]);
+		i++;
+	}
+	return (0);
 }
 
-int     main(int ac, char av)
+int	main(void)
 {
-    printf("%s", (char *)ft_memchr((char *)"HeLLO", 'e', 4));
-    return (0);
+	printf("%s", (char *)ft_memchr((char *)"HeLLO", 'e', 4));
+	return (0);
 }
