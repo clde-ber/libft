f958f9c357cdfd1b18432fdf71fea93903474cf9
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:38:53 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,33 +1,35 @@
 #include <string.h>
+#include <stdio.h>
 
-char    *ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    size_t i;
-    size_t j;
+	size_t i;
+	size_t j;
 
-    i = 0;
-    j = 0;
-    if (little[i] == 0)
-        return ((char *)&big[i]);
-    else
-    {
-        while (big[i + j] && i < len)
-        {
-            if (big[i + j] == little[j])
-            {
-                if (little[j] == '\0');
-                    return((char *)&big[i + j]);
-                j++;
-            }
-            i++;
-            j = 0;
-        }
-    }
-    return (0);
+	i = 0;
+	j = 0;
+	if (needle[i] == 0)
+		return ((char *)&haystack[i]);
+	else
+	{
+		while (haystack[i + j] && i < len)
+		{
+			if (haystack[i + j] == needle[j])
+			{
+				if (needle[j] == '\0')
+					return ((char *)&haystack[i + j]);
+				j++;
+			}
+			i++;
+			j = 0;
+		}
+	}
+	return (0);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf("%s", ft_strnstr(av[1], av[2], 5));
-    return (0);
+	(void)ac;
+	printf("%s", ft_strnstr(av[1], av[2], 5));
+	return (0);
 }
