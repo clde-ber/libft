bfa3b761400bd889e209423440811fdb3fa0987b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:34:51 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,31 +1,33 @@
 #include <string.h>
+#include <stdio.h>
 
-size_t      ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t i;
-    size_t j;
+	size_t i;
+	size_t j;
 
-    i = 0;
-    j = 0;
-    while (dst[i])
-    {
-        i++;
-        if (i == size)
-            return(size);
-    }
-    dst[i] = '\0';
-    while (src[j])
-    {
-        dst[i] = src[j];
-        i++;
-        j++;
-    }
-    dst[i] = '\0';
-    return (i);
+	i = 0;
+	j = 0;
+	while (dst[i])
+	{
+		i++;
+		if (i == dstsize)
+			return (dstsize);
+	}
+	dst[i] = '\0';
+	while (src[j])
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (i);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf("%d", ft_strlcat(av[2], av[1], 4));
-    return (0);
+	(void)ac;
+	printf("%zu", ft_strlcat(av[2], av[1], 4));
+	return (0);
 }
