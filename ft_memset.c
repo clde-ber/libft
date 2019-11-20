d7c70adc64e9df6051de7470c161c0cdd9fe26d3
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:31:14 +0100

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,21 +1,21 @@
 #include <string.h>
 
-void    *ft_memset (void *s, int c, size_t n)
+void	*ft_memset(void *b, int c, size_t len)
 {
-    size_t i;
-    
-    i = 0;
-    while (i < n)
-    {
-        ((char *)s)[i] = c + '0';
-        i++;
-    }
-    printf("%s", s);
-    return ((void *)s);
+	size_t i;
+
+	i = 0;
+	while (i < len)
+	{
+		((char *)b)[i] = c + '0';
+		i++;
+	}
+	return ((void *)b);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    ft_memset(av[1], 5, 4);
-    return (0);
+	(void)ac;
+	ft_memset(av[1], 5, 4);
+	return (0);
 }
