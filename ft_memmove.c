cd93c230a3c8f2de5739f6d92056c0e0529fc255
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:29:19 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,22 +1,24 @@
 #include <string.h>
+#include <stdio.h>
 
-void       *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-    size_t i;
-    char *tmp;
+	size_t	i;
+	char	*tmp;
 
-    i = 0;
-    tmp = (char *)src;
-    while (i < n)
-    {
-        ((char *)dest)[i] = tmp[i];
-        i++;
-    }
-    return (dest);
+	i = 0;
+	tmp = (char *)src;
+	while (i < len)
+	{
+		((char *)dst)[i] = tmp[i];
+		i++;
+	}
+	return (dst);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf("%s", (char *)ft_memmove(av[2], av[1], 5));
-    return (0);
+	(void)ac;
+	printf("%s", (char *)ft_memmove(av[2], av[1], 5));
+	return (0);
 }
