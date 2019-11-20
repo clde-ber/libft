053056e7c7e436abd3f51b02783949af45771b05
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:38:09 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,21 +1,23 @@
 #include <string.h>
+#include <stdio.h>
 
-int     ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (s1[i] || s2[i])
-    {
-        if ((s1[i] != s2[i]) || (i == n))
-            break;
-        i++;
-    }
-    return (s1[i] - s2[i]);
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if ((s1[i] != s2[i]) || (i == n))
+			break ;
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf("%d", ft_strncmp(av[1], av[2], 3));
-    return (0);
+	(void)ac;
+	printf("%d", ft_strncmp(av[1], av[2], 3));
+	return (0);
 }
