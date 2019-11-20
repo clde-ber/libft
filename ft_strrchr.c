6d35c744631fad7468dbd826b5f640d81fa2636d
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:40:14 +0100

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,30 +1,24 @@
-static int  ft_strlen(const char *s)
-{
-    int i;
+#include <stdio.h>
 
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (0);
 }
 
-char        *ft_strrchr(const char *s, int c)
+int	main(int ac, char **av)
 {
-    int i;
-
-    i = ft_strlen(s);
-    while (i >= 0)
-    {
-        if (s[i] == (char)c)
-        //  printf("%d", i);
-            return ((char *)&s[i]);
-        i--;
-    }
-    return (0);
-}
-
-int         main(int ac, char **av)
-{
-    printf("%s", ft_strrchr(av[1], av[2][0]));
-    return (0);
+	(void)ac;
+	printf("%s", ft_strrchr(av[1], av[2][0]));
+	return (0);
 }
