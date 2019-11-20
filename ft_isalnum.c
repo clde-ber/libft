c320d0a34ac8031ca4eb5f9333959167c1ec2a04
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-20 19:44:13 +0100

    Create ft_isalnum.c

---
+++ ft_isalnum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	ft_isalnum(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	printf("%d", ft_isalnum(48));
+	return (0);
+}
