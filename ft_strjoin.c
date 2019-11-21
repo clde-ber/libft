18f805ef5e286be97c9b1f378d67b0c4a1cb1592
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-21 14:12:19 +0100

    Create ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	if (!(ptr = malloc(sizeof(s1) + sizeof(s2))))
+		return (0);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	printf("%s", ft_strjoin(av[1], av[2]));
+	return (0);
+}
