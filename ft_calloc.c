40fcd89e0f23dd06207ad2cc90c4b101fab8af4c
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-22 11:13:05 +0100

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    size_t i;
-    void *ptr;
+	size_t	i;
+	void	*ptr;
 
-    i = 0;
-    if (!(ptr = (void *)malloc(nmemb * size)))
-        return (0);
-    while (i < nmemb)
-    {
-        ((char *)ptr)[i] = 0;
-        i++;
-    }
-    return (ptr);
+	i = 0;
+	if (!(ptr = (void *)malloc(count * size)))
+		return (0);
+	while (i < count)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
 }
 
-int     main(int ac, char **av)
+int	main(void)
 {
-    printf("%s", (char *)ft_calloc(5, 0));
-    return (0);
+	printf("%s", (char *)ft_calloc(5, 0));
+	return (0);
 }
