a9bf109df5a1dd70c4c6566c2ff2632609fd267b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-22 11:14:29 +0100

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,26 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-char *ft_strdup(const char *s)
+char	*ft_strdup(const char *s)
 {
-    size_t i;
-    char *ptr;
+	size_t	i;
+	char	*ptr;
 
-    i = 0;
-    if(!(ptr = (char *)malloc(sizeof(s))))
-        return (0);
-    while (s[i])
-    {
-        ptr[i] = s[i];
-        i++;
-    }
-    return (ptr);
+	i = 0;
+	if (!(ptr = (char *)malloc(sizeof(s))))
+		return (0);
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	return (ptr);
 }
 
-int     main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    printf((char *)ft_strdup(av[1]));
-    return (0);
+	(void)ac;
+	printf("%s", (char *)ft_strdup((const char *)av[1]));
+	return (0);
 }
