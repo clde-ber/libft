5a624633b53a5ef113c77b680629b26321f43db3
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-25 20:02:43 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -26,36 +26,6 @@ static size_t	is_in_set(char c, char const *set)
 	return (0);
 }
 
-static void		*find_malloc(char const *s1, char const *set)
-{
-	void *ptr;
-
-	if (is_in_set(s1[0], set) && is_in_set(s1[ft_strlen(&s1[0]) - 1], set))
-	{
-		if (!(ptr = malloc(sizeof(char const) * (ft_strlen(s1) - 1))))
-			return (0);
-		return (ptr);
-	}
-	else if (is_in_set(s1[0], set) && is_in_set(s1[ft_strlen(&s1[0]) - 1], set) == 0)
-	{
-		if (!(ptr = malloc(sizeof(char const) * (ft_strlen(s1) + 1))))
-			return (0);
-		return (ptr);
-	}
-	else if (is_in_set(s1[0], set) || is_in_set(s1[ft_strlen(&s1[0]) - 1], set))
-	{
-		if (!(ptr = malloc(sizeof(char const) * (ft_strlen(s1)))))
-			return (0);
-		return (ptr);
-	}
-	else
-	{
-		if (!(ptr = malloc(sizeof(char const) * (ft_strlen(s1) + 1))))
-			return (0);
-		return (ptr);
-	}
-}
-
 char		*ft_strtrim(char const *s1, char const *set)
 {
 	size_t i;
@@ -63,27 +33,51 @@ char		*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	char *str;
 
-	str = (char *)find_malloc(s1, set);
-	while (s1[i])
-	{
-		if (is_in_set(s1[0], set) && (i < (ft_strlen(s1))))
-		//	str[0] = s1[1];
-		else
-		//	str[0] = s1[i];
-		if (is_in_set(s1[ft_strlen(s1) - 1], set))
-		//	str[ft_strlen(s1) - 1] = '\0';
-		else
-		//	str[ft_strlen(s1) - 1] = s1[ft_strlen(s1) - 1];
-		else
-			str[i] = s1[i];
-		i++;
-	}
+	str = malloc(sizeof(char) * (ft_strlen(s1)));
+	if ((is_in_set(s1[0], set) && is_in_set(s1[ft_strlen(s1) - 1], set)))
+		{
+			while (i < ft_strlen(s1) - 2)
+			{
+				str[i] = s1[i + 1];
+				i++;
+			}
+			str[i] = '\0';
+		}
+	else if (is_in_set(s1[0], set)) 
+		{
+			i = 0;
+			while (i < ft_strlen(s1) - 1)
+			{
+				str[i] = s1[i + 1];
+				i++;
+			}
+			str[i] = '\0';
+		}
+	else if (is_in_set(s1[ft_strlen(s1) - 1], set))
+		{	
+			i = 0;
+			while (i < ft_strlen(s1) - 1)
+			{
+				str[i] = s1[i];
+				i++;
+			}
+			str[i] = '\0';
+		}
+	else
+		{
+			i = 0;
+			while (i < ft_strlen(s1))
+			{
+				str[i] = s1[i];
+				i++;
+			}
+		}
+		str[i] = '\0';
 	return (str);
 }
 
 int main (int ac, char **av)
 {
-	//printf("%lu", sizeof(char const));
 	printf("%s", ft_strtrim(av[1], av[2]));
 	return (0);
 }
