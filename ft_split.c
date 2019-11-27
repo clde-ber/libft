60ae7af6abe37f203e592849b4fb5c270262cfc6
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-27 17:12:19 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -14,61 +14,70 @@ size_t	ft_strlen(char const *s)
 
 static size_t count_malloc(char const *s, char c)
 {
-    size_t i;
-    size_t count;
+	size_t i;
+	size_t count;
 
-    i = 0;
-    count = 0;
-    while (s[i])
-    {
-        if (s[i] == c)
-                count++;
-        if (s[0] == c || s[(ft_strlen(s)) - 1] == c)
-            count -= 1;
-        if (s[0] == c && s[(ft_strlen(s)) - 1] == c)
-            count -= 2;
-        i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			count++;
+		if ((s[i] == c && i == 0) || s[(ft_strlen(s)) - 1] == c)
+			count -= 1;
+		if ((s[i] == c && i == 0) && s[(ft_strlen(s)) - 1] == c)
+			count -= 2;
+		i++;
+	}
+	return (count + 1);
 }
 
 size_t  cut_s(size_t i, char const *s, char c)
 {
-    while (s[i])
-    {
-        if(s[i] != c)
-            i++; 
-    }
-    return (i);
+	size_t	count;
+
+	while (s[i])
+	{
+		if (s[i] != c)
+			count++;
+		i++;
+	}
+	return (count);
 }
 
 char **ft_split(char const *s, char c)
 {
-    size_t i;
-    size_t j;
-    char **split;
+	size_t i;
+	size_t j;
+	size_t k;
+	char **split;
 
-    i = 0;
-    j = 0;
-    split = malloc(sizeof(char *) * (count_malloc(s, c) + 1));
-    while (split[i])
-    {
-        split[i] = (char *)malloc(sizeof(char) * j);
-        j += cut_s(j, s, c);
-        i++;
-    }
-    split[i] = 0;
-    return (split);
+	i = 0;
+	j = 0;
+	if (!(split = malloc(sizeof(char *) * (count_malloc(s, c) + 1))))
+		return (0);
+	while (s1[i])
+	{
+		if (!(split[i] = malloc(sizeof(char) * k)))
+			return (0);
+		k = cut_s(k, s, c);
+		i++;
+		printf("%li", j);
+	}
+	split[i] = 0;
+	return (split);
 }
 
 int     main(int ac, char **av)
 {
-    size_t i;
+	printf("%s", ft_split(av[1], 'c')[1]);
+//	size_t i;
 
-    i = 0;
-    while (ft_split(av[1], 'c')[i])
-    {
-            printf("%s", ft_split(av[1], 'c')[1]);
-            i++;
-    }
+//	i = 1;
+//	while ((ft_split(av[1], 'c'))[i])
+//	{
+//		printf("%s", (ft_split(av[1], 'c'))[i]);
+//		i++;
+//	}
+	return (0);
 }
