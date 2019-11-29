d14d989be9122cfa39cf9d8bc52c0a7b44dfaa39
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-11-29 17:06:04 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -26,6 +26,7 @@ static size_t count_malloc(char const *s, char c)
 			count++;
 		i++;
 	}
+	printf("Ft_Count - Nombre de mots : %zu\n", count);
 	return (count);
 }
 
@@ -34,56 +35,81 @@ size_t  cut_s(size_t i, char const *s, char c)
 	size_t	count;
 
 	count = 0;
-	while (s[i] == c)
+	printf("Ft_Cut_s - 1 - La chaine recu est = %s\n", s);
+	while (s[i] == c && s[i])
 		i++;
-	while (s[i] != c)
+	while (s[i] != c && s[i])
 	{
 		count++;
 		i++;
 	}
+	printf("Ft_Cut_s - 2 - La taille du mot est  = %d\n", count);
 	return (count);
 }
 
-char *fill_split(char *split, char const *s)
+char *fill_split(size_t i, char *split, char const *s, char c)
 {
-	size_t i;
+	size_t j;
 	size_t k;
 	char *str;
 
-	while (split[i][k])
+	j = 0;
+	k = 0;
+	while (s[j])
 	{
-
+		if (s[j] != c)
+		{
+			str[k] = s[j];
+			k++;
+		}
+		j++;
 	}
+	str[k] = '\0';
 	return (str);
 }
 
 char **ft_split(char const *s, char c)
 {
 	size_t i;
+	size_t i2;
 	size_t j;
 	size_t k;
 	char **split;
 
 	i = 0;
+	i2 = 0;
 	j = 0;
 	k = cut_s(i, s, c);
 	if (!(split = malloc(sizeof(char *) * (count_malloc(s, c)))))
 		return (0);
-	split[count_malloc(s, c)] = 0;
-	while (split[j])
+	while (j < count_malloc(s, c) - 1)
 	{
-		if (!(split[j] = malloc(sizeof(char) * k)))
+		if (!(split[j] = malloc(sizeof(char) * (k + 1))))
 			return (0);
-		k = cut_s(k, s, c);
-		split = fill_split(i, split[j], s);
-
+		while (s[i])
+		{
+			printf("Ft_split - 3 - Boucle While\n");
+			if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+				break ;
+			i++;
+		}
+		i += 1;
+		printf("Valeur de i apres la boucle - %lu\n", i);
+		k = cut_s(i, s, c);
+		printf("Ft_Split - 1 - Valeur de K dans boucle =  %lu\n", k);
+		printf("Ft_Split - 2 - s =  %lu\n", k);
+	//	split[j] = fill_split(i, split[j], s, c);
+		j++;
+		printf("valeur de j - %lu\n", j);
 	}
+	split[j - 1] = 0;
 	return (split);
 }
 
-int     main(int ac, char **av)
+int     main(void)
 {
-	printf("%li", count_malloc("cchellocctoiccsalut", 'c'));
-	printf("%li", cut_s(0, "cchellocctoiccsalut", 'c'));
+	//printf("Fonction Count Malloc = %li\n", count_malloc("cchellocctoiccsalut", 'c'));
+	//printf("Fonction cut_s = %li\n", cut_s(0, "cchellocctoiccsalut", 'c'));
+	ft_split("cchellocctoiccsalut", 'c');
 	return (0);
 }
