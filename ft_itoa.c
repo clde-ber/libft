e3f989833c9240d3883bb105fe35f60882fe0012
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-03 14:43:55 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -18,14 +18,18 @@ static int ft_strlen_int(int n)
 	if (n <= 2147483647 && n >= -2147483648)
 	{
 		if (n < 0)
+		{
 			n = -n;
+			i+=1;
+		}
 		while (n > 0)
 		{
 			n = n / 10;
 			i++;
 		}
 	}
-	return (i + 1);
+	printf("\n valeur de ft_strlen_int %d", (i));
+	return (i);
 }
 
 char *ft_itoa(int n)
@@ -39,40 +43,51 @@ char *ft_itoa(int n)
 	i = 0;
 	j = 0;
 	k = n;
-	str = malloc(sizeof(char) * ft_strlen_int(n) + 1);
+	if (!(str = malloc(sizeof(char) * ft_strlen_int(n) + 1)))
+			return (0);
 	revstr = malloc(sizeof(str));
 	if (n <= 2147483647 && n >= -2147483648)
 	{
-		if (n < 0)
-			n = -n;
-		while (j++ <= ft_strlen_int(n) + 1)
+		if (n == -2147483648)
 		{
-			str[i++] = (n % 10) + '0';
+			revstr = "-2147483648";
+			return (revstr);
+		}
+		else if (n < 0)
+			n = -n;
+		while (j++ < ft_strlen_int(k))
+		{
+			str[i++] = n % 10 + '0';
 			n = n / 10;
-			printf("%c", 'a');
+			printf("\n%c", 'a');
 		}
 		printf("\n valeur de str au debut %s", str);
-		j = ft_strlen_int(k) - 2;
+		j = ft_strlen_int(k) - 1;
 		i = 0;
 		while (i < ft_strlen_int(k) / 2)
-		{
 			revstr[i++] = str[j--];
-		}
 		printf("\n valeur de i %d", i);
 		printf("\n valeur de j %d", j);
 		printf("\n valeur de str %s", str);
+		printf("\n valeur de rev str %s", revstr);
 		while (i < ft_strlen_int(k))
 			revstr[i++] = str[j--];
+		printf("\n valeur de rev str %s", revstr);
 		printf("\n\n\n valeur de i a la fin %d", i);
-		revstr[i - 1] = '\0';
-
-
+		revstr[i] = '\0';
 	}
+//	if (n <= 2147483647 && n >= -2147483648 && k < 0)
+//	{
+//		i = 1;
+//		revstr[i - 1] = '-';
+//		while (i++ < ft_strlen_int(k))
+//			revstr[i] = revstr[i + 1];
+//	}
 	return (revstr);
 }
 
 int	main(void)
 {
-	printf("          \n%s", ft_itoa(1872));
+	printf("          \n%s", ft_itoa(2147483647));
 	return (0);
 }
