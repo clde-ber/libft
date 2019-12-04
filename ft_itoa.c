9bc172e11a056f853f92613ec95fb32472ab1678
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-04 19:45:13 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -24,6 +24,21 @@ static int	len_int(int n)
 	return (i);
 }
 
+static int	ft_pow(int nb, int pow)
+{
+	int i;
+	int res;
+
+	i = 0;
+	res = 1;
+	while (i < pow)
+	{
+		res = res * nb;
+		i++;
+	}
+	return (res);
+}
+
 char	*ft_itoa(int n)
 {
 	int	i;
@@ -35,15 +50,16 @@ char	*ft_itoa(int n)
 	i = 0;
 	j = 0;
 	k = n;
-	if (!(str = malloc(sizeof(char) * len_int(n) + 1)) || !(revstr = malloc(sizeof(str))))
+	if (!(str = malloc(sizeof(char) * len_int(n) + 1))
+	|| !(revstr = malloc(sizeof(str))))
 		return (0);
 	n = (n < 0 && k != -2147483648) ? -n : n;
 	while (j++ < len_int(k) && k != -2147483648)
 	{
-		str[i++] = n % 10 + '0';
-		n = n / 10;
+	//	printf("valeur de n : ft_pow \n%d\n", ft_pow(10, i));
+		str[i++] = ((n / ft_pow(10, i)) % 10) + '0';
 	}
-	printf("taille du mot %d\n", i);
+	printf("\ntaille du mot %d\n", i);
 	printf("valeur de len_int(k) soit longueur du mot %d\n\n", len_int(k));
 	while ((i - len_int(k) <= len_int(k) / 2) && k != -2147483648)
 		revstr[i++ - len_int(k)] = str[--j - 1];
@@ -51,6 +67,7 @@ char	*ft_itoa(int n)
 	printf("valeur de j apres la premiere boucle qui inverse %d\n\n", (j -1));
 	while ((i - len_int(k) <= len_int(k)) && k != -2147483648)
 		revstr[i++ - len_int(k) - 1] = str[--j];
+	revstr[i - len_int(k) - 1] = '\0';
 	printf("valeur de i apres la seconde boucle qui poursuit %d\n", (i - len_int(k) - 1));
 	printf("valeur de j apres la seconde boucle qui poursuit %d\n\n", (j - 1));
 	revstr[0] = (k < 0 && k != -2147483648) ? '-' : revstr[0];
@@ -60,6 +77,6 @@ char	*ft_itoa(int n)
 
 int	main(void)
 {
-	printf("          \n%s", ft_itoa(-2174));
+	printf("          \n%s", ft_itoa(-21474));
 	return (0);
 }
