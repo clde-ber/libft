c12d1c0dad3de2f09d6b3927343f4a854b0bb225
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:59:26 +0100

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
-
 int	ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (c + 32);
 	return (c);
-}
-
-int	main(void)
-{
-	printf("%c", ft_tolower('Z'));
-	return (0);
 }
