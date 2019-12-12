34e586fb517440ec4b1f9b7f4ed09d94c73801fe
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:50:56 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,12 +1,4 @@
-#include <unistd.h>
-#include <string.h>
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, sizeof(char));
-}
-
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
 	size_t i;
 
@@ -16,15 +8,4 @@ void ft_putstr_fd(char *s, int fd)
 		ft_putchar_fd(s[i], fd);
 		i++;
 	}
-}
-
-int	main(void)
-{
-	int fd;
-	char *str;
-
-	fd = 1;
-	str = "hello";
-	ft_putstr_fd(str, fd);
-	return (0);
 }
