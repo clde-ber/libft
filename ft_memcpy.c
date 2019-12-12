5db72a43c212b7fe4dbdf23ef1110663e77c8022
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:43:33 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t i;
@@ -11,13 +8,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		((char *)dst)[i] = ((char *)src)[i];
 		i++;
 	}
-	printf("%s", dst);
 	return ((void *)dst);
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	ft_memcpy(av[2], av[1], 4);
-	return (0);
 }
