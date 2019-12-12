91d8c6ab396299364ff2b0ff56822c57d11c478b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:36:53 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-
 void	ft_bzero(void *s, size_t n)
 {
 	size_t i;
@@ -11,12 +8,4 @@ void	ft_bzero(void *s, size_t n)
 		((char *)s)[i] = '\0';
 		i++;
 	}
-	printf("%s", s);
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	ft_bzero(av[1], 4);
-	return (0);
 }
