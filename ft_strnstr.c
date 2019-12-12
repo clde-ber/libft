5f496cd2d4e57d9540376a44faa3dd19b261437e
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 18:09:12 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -2,24 +2,25 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t i;
 	size_t j;
+	size_t k;
 
 	i = 0;
 	j = 0;
-	if (needle[i] == 0)
-		return ((char *)&haystack[i]);
-	else
+	k = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i + k])
 	{
-		while (haystack[i + j] && i < len)
+		while (needle[j] && j < len)
 		{
-			if (haystack[i + j] == needle[j])
-			{
-				if (needle[j] == '\0')
-					return ((char *)&haystack[i + j]);
-				j++;
-			}
-			i++;
-			j = 0;
+			if (haystack[i + k] == needle[j])
+				k++;
+			if (!needle[j])
+				return ((char *)&haystack[i + k]);
+			j++;
 		}
+		i++;
+		j = 0;
 	}
 	return (0);
 }
