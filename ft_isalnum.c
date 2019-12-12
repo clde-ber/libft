5886f6e7f4110d36f90f80d9e64373e1c6a622c5
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:38:00 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int	ft_isalnum(int c)
 {
 	if (c >= 'a' && c <= 'z')
@@ -8,11 +6,5 @@ int	ft_isalnum(int c)
 		return (1);
 	else if (c >= '0' && c <= '9')
 		return (1);
-	return (0);
-}
-
-int	main(void)
-{
-	printf("%d", ft_isalnum(48));
 	return (0);
 }
