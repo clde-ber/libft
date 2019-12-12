dac29259c546a2c2360b28c6ce7f8f2abae98c0b
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:55:24 +0100

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,6 +1,3 @@
-#include <string.h>
-#include <stdio.h>
-
 size_t	ft_strlen(const char *s)
 {
 	size_t i;
@@ -9,11 +6,4 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	printf("%zu", ft_strlen(av[1]));
-	return (0);
 }
