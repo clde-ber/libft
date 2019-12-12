38995e2540a938c622f4487824d7dd1a6ad27794
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:57:41 +0100

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
@@ -13,12 +11,5 @@ char	*ft_strrchr(const char *s, int c)
 			return ((char *)&s[i]);
 		i--;
 	}
-	return (0);
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	printf("%s", ft_strrchr(av[1], av[2][0]));
 	return (0);
 }
