280327a59c87281105608983b3c7ef43a69c24d6
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:42:53 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,22 +1,13 @@
-#include <string.h>
-#include <stdio.h>
-
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while ((((char *)s1)[i] || ((char *)s2)[i]) && i < n)
+	while ((((unsigned char *)s1)[i] || ((unsigned char *)s2)[i]) && i < n)
 	{
-		if (((char *)s1)[i] != ((char *)s2)[i])
-			break ;
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	return (((char *)s1)[i] - ((char *)s2)[i]);
-}
-
-int	main(void)
-{
-	printf("%d", ft_memcmp((char *)"Hello", (char *)"Helloa", 3));
 	return (0);
 }
