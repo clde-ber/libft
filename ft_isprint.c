999943b83ffe70c58342f6e9bfd93f070bd5d569
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:40:16 +0100

    Update ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
-
 int	ft_isprint(int c)
 {
-	if (c >= 40 && c <= 176)
+	if (c >= '\040' && c <= '\176')
 		return (1);
-	return (0);
-}
-
-int	main(void)
-{
-	printf("%d", ft_isprint('5'));
 	return (0);
 }
