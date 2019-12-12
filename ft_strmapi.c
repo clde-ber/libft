44505ecd679e9ddacba29cd250c62046d72c5457
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:56:00 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,31 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-size_t	ft_strlen(const char *s)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t i;
+	size_t	i;
+	char	*str;
 
 	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-static char	ft_fill_w_a(unsigned int a, char b)
-{
-	a = 5;
-	b = 'a';
-	return (b);
-}
-
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	size_t i;
-	char *str;
-
-	i = 0;
-	printf("%lu", sizeof(s));
 	if (!(str = malloc(sizeof(s))))
 		return (0);
 	while (i < ft_strlen(s))
@@ -35,12 +13,4 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	}
 	str[i] = '\0';
 	return (str);
-}
-
-int main (int ac, char **av)
-{
-	(void)ac;
-
-	printf("\n%s\n", ft_strmapi(av[1], ft_fill_w_a));
-	return (0);
 }
