69569604e3b2780447c8f386e286e18e306fbddb
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:59:52 +0100

    Update ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,15 +1,7 @@
-#include <stdio.h>
-
 int	ft_toupper(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	else
 		return (c);
-}
-
-int	main(void)
-{
-	printf("%d", ft_toupper('z'));
-	return (0);
 }
