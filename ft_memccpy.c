3dece194520bb02123738f107f08160965a4fc24
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-12 10:41:32 +0100

    Update ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,22 +1,14 @@
-#include <string.h>
-#include <stdio.h>
-
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t i;
 
 	i = 0;
-	while ((i < n) && (((char *)dst)[i] != c))
+	while (i < n)
 	{
 		((char *)dst)[i] = ((char *)src)[i];
+		if (((char *)src)[i] == (unsigned char)c)
+			return (&dst[i + 1]);
 		i++;
 	}
-	return ((void *)dst);
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	printf("%s", (char *)ft_memccpy(av[2], av[1], 'e', 4));
-	return (0);
+	return (NULL);
 }
