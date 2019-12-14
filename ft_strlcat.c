3130ec1fb4ce804ce4b786af0993772987c24be5
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-14 18:51:00 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,23 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t j;
+	size_t len_dst;
+	size_t len_src;
 
-	i = 0;
-	j = 0;
-	while (dst[i])
+	if (dst != NULL && src != NULL)
 	{
-		i++;
-		if (i == dstsize)
-			return (dstsize);
+		i = 0;
+		len_dst = ft_strlen((const char *)dst);
+		len_src = ft_strlen(src);
+		if (dstsize < len_dst)
+			return (len_src + dstsize);
+		while (src[i] && len_dst + i < dstsize - 1)
+		{
+			dst[len_dst + i] = src[i];
+			i++;
+		}
+		dst[len_dst + i] = '\0';
+		return (len_dst + len_src);
 	}
-	dst[i] = '\0';
-	while (src[j])
-	{
-		dst[i] = src[j];
-		i++;
-		j++;
-	}
-	dst[i] = '\0';
-	return (i);
+	return (0);
 }
