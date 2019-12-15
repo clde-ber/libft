196eb39c87b5e82fd46c6bffd9f4090db63c8374
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-15 14:18:48 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,24 +2,17 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*tmp_src;
 	unsigned char	*tmp_dst;
-	size_t			i;
-	size_t			j;
 
-	tmp_src = (unsigned char *)src;
-	tmp_dst = (unsigned char *)dst;
-	i = 0;
-	j = 0;
-	if (!len || (dst == src) || !dst || !src)
-		return (dst);
-	while (i < len && tmp_src[len - i] == tmp_dst[i])
+	if (!dst && !src)
+		return (NULL);
+	if (src <= dst)
 	{
-		*tmp_dst++;
-		i++;
+		tmp_dst = (unsigned char *)dst;
+		tmp_src = (unsigned char *)src;
+		while (len--)
+			tmp_dst[len] = tmp_src[len];
 	}
-	while (j++ < len)
-	{
-		tmp_dst[j] = tmp_src[j];
-		j++;
-	}
-	return (tmp_dst);
+	else
+		ft_memcpy(dst, src, len);
+	return (dst);
 }
