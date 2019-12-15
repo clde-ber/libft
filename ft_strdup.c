f733f5eff4edcb91a3a362b0176e9b96f254c44f
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-15 14:23:20 +0100

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -4,12 +4,13 @@ char	*ft_strdup(const char *s)
 	char	*ptr;
 
 	i = 0;
-	if (!(ptr = (char *)malloc(sizeof(s))))
+	if (!(ptr = (char *)malloc(ft_strlen(s) + 1)))
 		return (0);
 	while (s[i])
 	{
 		ptr[i] = s[i];
 		i++;
 	}
+	ptr[i] = '\0';
 	return (ptr);
 }
