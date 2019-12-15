90a9de230ae7b6c6ecab73a27988cde39c410124
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-15 14:22:46 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,13 +1,9 @@
 char	*ft_strchr(const char *s, int c)
 {
-	int i;
+	char	*str;
 
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == (char)c)
-			return ((char *)&s[i]);
-		i++;
-	}
-	return (0);
+	str = (char *)s;
+	while (*str && *str != c)
+		str++;
+	return (*str == c ? str : NULL);
 }
