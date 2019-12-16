f38e1382f607a8f12e153aa31ce61ed5a9814d34
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-16 15:26:10 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,7 +1,28 @@
 void	ft_putnbr_fd(int n, int fd)
 {
-	char *number;
+	char c;
 
-	number = ft_itoa(n);
-	ft_putstr_fd(number, fd);
+		if (n == -2147483648)
+			write(fd, "-2147483648", 12);
+		else if (n < 0)
+		{
+			n = -n;
+			write (fd, "-", 1);
+		}
+		if (n >= 0 && n <= 9)
+		{
+			c = (n % 10) + '0';
+			write(fd, &c, 1);
+		}
+		else if (n > 9)
+		{
+			if (n > 0)
+				ft_putnbr_fd((n / 10), fd);
+		}
+}
+
+int main()
+{
+	ft_putnbr_fd(2147, 1);
+	return 0;
 }
