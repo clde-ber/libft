b8d886dde66df1ec724aebb15d1daf053368c18f
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-21 14:10:09 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -2,97 +2,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static size_t	len_wd(char const *str, int c)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			break;
+		i++;
+	}
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+
+	if (!dst && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+	return ((void *)dst);
+}
+
 static size_t	count_malloc(char const *s, char c)
 {
 	size_t i;
 	size_t count;
 
 	i = 0;
-	count = 1;
+	count = 0;
 	while (s[i])
 	{
-		if (s[i] != c && (s[i + 1] == c || s[i] == '\0'))
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 			count++;
 		i++;
 	}
 	return (count);
 }
 
-static size_t	cut_s(size_t i, char const *s, char c)
-{
-	size_t	count;
-
-	count = 0;
-	while (s[i] == c && s[i])
-		i++;
-	while (s[i] != c && s[i])
-	{
-		count++;
-		i++;
-	}
-	return (count);
-}
-
-static char		*fill_split(char *str, size_t i, char const *s, char c)
-{
-	size_t k;
-
-	k = 0;
-	while (s[i] == c && s[i])
-		i++;
-	while (s[i] != c && s[i])
-	{
-		if (s[i] != c)
-		{
-			str[k] = s[i];
-			k++;
-		}
-		i++;
-	}
-	str[k] = '\0';
-	return (str);
-}
-
 char			**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
-	size_t	l;
-	char	**split;
+	char **res;
+	size_t str_nb;
 
 	i = 0;
-	j = -1;
-	l = 0;
-	if (!(split = malloc(sizeof(char *) * (count_malloc(s, c)))))
+	j = 0;
+	str_nb = count_malloc(s,c);
+	printf("str_nb %zu\n", str_nb);
+	if (!(res = malloc(sizeof(char *) * str_nb)))
 		return (0);
-	while (++j < count_malloc(s, c) - 1)
+	while (s[i] && j < str_nb)
 	{
-		k = cut_s(i, s, c);
-		if (!(split[j] = malloc(sizeof(char) * k + 1)))
-			return (0);
-		while (s[i++])
-			if (s[i - 1] != c && (s[i] == c || s[i] == '\0'))
-				break ;
-//		i = (s[i]) ? i + 1 : i;
-		split[j] = fill_split(split[j], l, s, c);
-		l = i;
+		if (s[i] != c)
+		{
+			if (!(res[j] = malloc(sizeof(char) * (len_wd(&s[i], c) + 1))))
+				return (0);
+			res[j] = ft_memcpy(res[j], &s[i], len_wd(&s[i], c) + 1);
+			res[j][len_wd(&s[i], c)] = '\0';
+			j++;
+			i += len_wd(&s[i], c);
+		}
+		i++;
 	}
-	split[j] = 0;
-	return (split);
+	res[str_nb] = 0;
+	return (res);
 }
 
 int main()
 {
 	int i;
-	char const *s = "M-A-K-E-L-I-B-F-T-G-R-E-A-T-A-G-A-I-N-!-";
+	char const *s = "M-AAAA-K-E----L-I-B-FFFF-T-G-R-E-A-T-A-G-A-I-N---";
 	char c = '-';
 
 	i = 0;
-	while (i < 22)
+	while (i < 21)
 	{
 		printf("%s\n", ft_split(s, c)[i]);
 		i++;
 	}
+//	printf("%s\n", ft_split(s, c)[19]);
+
 	return (0);
 }
