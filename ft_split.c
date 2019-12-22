1a729d23fa588582c8253cbb113be4aea6f8e944
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-22 18:13:36 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -37,11 +37,12 @@ char			**ft_split(char const *s, char c)
 
 	i = -1;
 	j = 0;
+	if (!s || !c)
+		return (0);
 	str_nb = count_malloc(s, c);
 	if (!(res = malloc(sizeof(char *) * (str_nb + 1))))
 		return (0);
-	while (s[i++])
-	{
+	while (s[i++ + 1])
 		if (s[i] != c && j < str_nb)
 		{
 			if (!(res[j] = malloc(sizeof(char) * (len_wd(&s[i], c) + 1))))
@@ -51,7 +52,6 @@ char			**ft_split(char const *s, char c)
 			j++;
 			i += len_wd(&s[i], c);
 		}
-	}
 	res[str_nb] = 0;
 	return (res);
 }
