474f9d62df4b8302a6c3cc2fa1fdb0aba7548296
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-22 18:15:24 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,6 +6,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if (!s1 || !s2)
+		return (0);
 	if (!(ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)))))
 		return (0);
 	while (s1[i])
