e8583661ffab3506a823ad17d60563d87131d132
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-22 18:08:32 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -5,7 +5,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (((char *)s)[i] == c)
+		if ((unsigned char)((char *)s)[i] == (unsigned char)c)
 			return ((void *)&s[i]);
 		i++;
 	}
