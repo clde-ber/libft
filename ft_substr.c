f8ec030e609c66b9b988753e57f71105659231f9
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-23 19:28:09 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -11,16 +11,9 @@ char		*ft_substr(char const *s, unsigned int start, size_t len)
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
 	if (len >= ft_strlen(s + start))
-	{
 		len = ft_strlen(s + start);
-		if (!(str = malloc(sizeof(char) * len)))
-			return (0);
-	}
-	else
-	{
-		if (!(str = malloc(sizeof(char) * (len + 1))))
-			return (0);
-	}
+	if (!(str = malloc(sizeof(char) * (len + 1))))
+		return (0);
 	while (j < len)
 		str[j++] = (char)s[i++];
 	str[j] = '\0';
