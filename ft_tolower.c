0bfdfe918cacb7d33e77fb0c2ff9e2e14562c551
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-24 16:19:14 +0100

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,6 +1,7 @@
-int	ft_tolower(int c)
+int	ft_toupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (c + 32);
-	return (c);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	else
+		return (c);
 }
