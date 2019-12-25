b97b7e0f33b82a6dee99aace5e77fa5954b59a10
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-25 16:13:50 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -15,28 +15,25 @@ static size_t	is_in_set(char const c, char const *set)
 char			*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	j;
-	size_t	k;
+	size_t	rm_len;
 	size_t	len;
-	char	*str;
 
 	i = 0;
-	k = 0;
-	len = (!s1 || !set) ? 0 : ft_strlen(s1);
-	j = len;
+	rm_len = 0;
 	if (!s1 || !set)
 		return (0);
-	while (i < j && is_in_set(s1[i], set))
-		i++;
-	while (j >= 0 && is_in_set(s1[--j], set))
-		k++;
-	if (!(str = (k == i && k != 0 && i != 0) ? malloc(sizeof(char))
-				: malloc(sizeof(char) * (len - i - k + 1))))
-		return (0);
-	j = len - k;
-	k = len - i - k;
-	str[k] = '\0';
-	while (j-- > i)
-		str[--k] = (char)s1[j];
-	return (str);
+	len = ft_strlen(s1) - 1;
+	if (s1 == set)
+		ft_strdup(s1);
+	else
+	{
+		while (s1[i] && is_in_set(s1[i], set))
+			i++;
+		while (len > 0 && s1[len] && is_in_set(s1[len], set))
+		{
+			rm_len++;
+			len--;
+		}
+	}
+	return (ft_substr(s1, i, ft_strlen(s1 + i) - rm_len));
 }
