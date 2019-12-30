ffe5cb4f3f24a5f6fad4dfed1e00274f4dfd19be
clde-ber <57361517+clde-ber@users.noreply.github.com>
2019-12-30 17:42:25 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -8,20 +8,12 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		return (0);
 	i = 0;
 	size = ft_strlen(s);
-	if (size == 0)
+	if (!(str = malloc(sizeof(char) * (size + 1))))
+		return (0);
+	while (i < size)
 	{
-		if (!(str = malloc(sizeof(char))))
-			return (0);
-	}
-	else
-	{
-		if (!(str = malloc(sizeof(char) * (size + 1))))
-			return (0);
-		while (i < size)
-		{
-			str[i] = (*f)(i, (char)s[i]);
-			i++;
-		}
+		str[i] = (*f)(i, s[i]);
+		i++;
 	}
 	str[i] = '\0';
 	return (str);
