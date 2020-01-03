a3f11506869fd8991c1894adc491227f635dce02
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-03 13:42:26 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,11 +3,12 @@ void	ft_putstr_fd(char *s, int fd)
 	size_t i;
 
 	i = 0;
-	if (!s)
-		return ;
-	while (s[i])
+	if (s != NULL)
 	{
-		ft_putchar_fd(s[i], fd);
-		i++;
+		while (s[i])
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
 	}
 }
