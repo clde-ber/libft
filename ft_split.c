08dbe67a57cc61dd93c22c22fb5e53026ba9898f
clde-ber <57361517+clde-ber@users.noreply.github.com>
2020-01-03 13:43:26 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -19,6 +19,8 @@ static size_t	count_malloc(char const *s, char c)
 
 	i = 0;
 	count = 0;
+	if (s[i] == '\0' || c == '\0')
+		return (1);
 	while (s[i])
 	{
 		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
@@ -42,31 +44,42 @@ static void		*ft_free(char **res, int j)
 	return (NULL);
 }
 
+static int		check_empty(char const *s, char **res, char c)
+{
+	if (s[0] == '\0' || c == '\0')
+	{
+		res[0] = (c == '\0') ? ft_strdup(s) : NULL;
+		res[1] = NULL;
+		return (1);
+	}
+	return (0);
+}
+
 char			**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
 	char	**res;
-	size_t	str_nb;
 
-	i = -1;
+	i = 0;
 	j = 0;
-	if (!s)
+	if (!s || !*s)
+		return ((char **)ft_calloc(2, sizeof(char *)));
+	if (!(res = malloc(sizeof(char *) * (count_malloc(s, c) + 1))))
 		return (0);
-	str_nb = (c == 0) ? 1 : count_malloc(s, c);
-	if (!(res = malloc(sizeof(char *) * (str_nb + 1))))
-		return (0);
-	res[0] = (c == 0) ? ft_strdup(s) : res[0];
-	while ((i++ + 1) < ft_strlen(s))
-		if (s[i] != c && j < str_nb)
+	while (i < ft_strlen(s))
+	{
+		while (j < count_malloc(s, c) && s[i] && s[i] != c)
 		{
 			if (!(res[j] = malloc(sizeof(char) * (len_wd(&s[i], c) + 1))))
 				return (ft_free(res, j));
-			res[j] = ft_memcpy(res[j], &s[i], len_wd(&s[i], c) + 1);
+			res[j] = ft_memmove(res[j], &s[i], len_wd(&s[i], c) + 1);
 			res[j][len_wd(&s[i], c)] = '\0';
 			j++;
 			i += len_wd(&s[i], c);
 		}
-	res[str_nb] = 0;
-	return (res);
+		i++;
+	}
+	res[count_malloc(s, c)] = 0;
+	return (check_empty(s, res, c)) ? res : res;
 }
